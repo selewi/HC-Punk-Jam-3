4e3ca9a795641667d97a9ef7fc3d1ae2816e3b13
Tomás Esconjaureguy <esconjaureguy470@gmail.com>
2021-09-10 18:03:01 -0300

    Add character description prefab

---
+++ Assets/Game/Scripts/Gameplay/Cards/CharacterDescriptionDisplayable.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using TMPro;
+
+namespace HCPJ3
+{
+    public class CharacterDescriptionDisplayable : MonoBehaviour
+    {
+        [SerializeField] private TextMeshPro _descriptionTMP = null;
+
+        public void SetText (string newText)
+        {
+            _descriptionTMP.text = newText;
+        }
+    }
+}
