3f1a3cbbc14a55f6615b0230f3b9d71ff1fad816
Tomás Esconjaureguy <esconjaureguy470@gmail.com>
2021-09-10 19:33:14 -0300

    Add score manager prefab

---
+++ Assets/Game/Scripts/Gameplay/Scoring/ScoreDisplayable.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using TMPro;
+
+namespace HCPJ3
+{
+    public class ScoreDisplayable : MonoBehaviour
+    { 
+        [SerializeField] private TextMeshPro _valueTMP = null;
+
+        private string _baseText = null;
+
+        private void Awake ()
+        {
+            _baseText = _valueTMP.text;
+        }
+
+        public void SetValue (int score)
+        {
+            _valueTMP.text = _baseText.Replace ("{value}", score.ToString ());
+        }
+    }
+}
---
+++ Assets/Game/Scripts/Gameplay/Scoring/ScoreManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using NaughtyAttributes;
+
+namespace HCPJ3
+{
+    public class ScoreManager : MonoBehaviour
+    {
+        [SerializeField] [ReadOnly] private int _currentScore = 0;
+
+        private ScoreDisplayable _displayable = null;
+
+        private void Awake ()
+        {
+            _displayable = GetComponentInChildren<ScoreDisplayable> ();
+        }
+
+        private void Start ()
+        {
+            _displayable.SetValue (_currentScore);
+        }
+
+        public void AddScore (int addedScore)
+        {
+            _currentScore += addedScore;
+            _displayable.SetValue (_currentScore);
+        }
+    }
+}
