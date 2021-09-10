500fa69e2d43ecee65727a01b091f8409c8876d8
Tomás Esconjaureguy <esconjaureguy470@gmail.com>
2021-09-10 17:16:14 -0300

    Add Portrait prefab

---
+++ Assets/Game/Scripts/Gameplay/Cards/PortraitController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NaughtyAttributes;
+
+public class PortraitController : MonoBehaviour
+{
+    [SerializeField] private Sprite[] _backgroundCollection = { };
+    [SerializeField] private Sprite[] _faceShapeCollection = { };
+    [SerializeField] private Sprite[] _hairCollection = { };
+    [SerializeField] private Sprite[] _eyesCollection = { };
+    [SerializeField] private Sprite[] _noseCollection = { };
+    [SerializeField] private Sprite[] _mouthCollection = { };
+
+    private PortraitDisplayable _displayable;
+
+    private void Awake ()
+    {
+        Initialize ();
+    }
+
+    private void Initialize ()
+    {
+        _displayable = GetComponentInChildren<PortraitDisplayable> ();
+    }
+
+    [Button ("Randomize test")]
+    public void Randomize ()
+    {
+        if (_displayable == null)
+        {
+            Initialize ();
+        }
+
+        _displayable.SetCharacterSprites (
+            GetRandomSprite (_backgroundCollection),
+            GetRandomSprite (_faceShapeCollection),
+            GetRandomSprite (_hairCollection),
+            GetRandomSprite (_eyesCollection),
+            GetRandomSprite (_noseCollection),
+            GetRandomSprite (_mouthCollection)
+        ); ;
+    }
+
+    private Sprite GetRandomSprite (Sprite[] spriteArray)
+    {
+        return spriteArray[Random.Range (0, spriteArray.Length)];
+    }
+
+}
---
+++ Assets/Game/Scripts/Gameplay/Cards/PortraitDisplayable.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortraitDisplayable : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer _backgroundSpriteRenderer = null;
+    [SerializeField] private SpriteRenderer _faceShapeSpriteRenderer = null;
+    [SerializeField] private SpriteRenderer _hairSpriteRenderer = null;
+    [SerializeField] private SpriteRenderer _eyesSpriteRenderer = null;
+    [SerializeField] private SpriteRenderer _noseSpriteRenderer = null;
+    [SerializeField] private SpriteRenderer _mouthSpriteRenderer = null;
+
+    public void SetCharacterSprites (Sprite backgroundSprite, Sprite faceShapeSprite, Sprite hairSprite, Sprite eyesSprite, Sprite noseSprite, Sprite mouthSprite)
+    {
+        _backgroundSpriteRenderer.sprite = backgroundSprite;
+        _faceShapeSpriteRenderer.sprite = faceShapeSprite;
+        _hairSpriteRenderer.sprite = hairSprite;
+        _eyesSpriteRenderer.sprite = eyesSprite;
+        _noseSpriteRenderer.sprite = noseSprite;
+        _mouthSpriteRenderer.sprite = mouthSprite;
+    }
+
+}
